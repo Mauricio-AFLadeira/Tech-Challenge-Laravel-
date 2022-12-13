f5e84ed4ffc39f9af4aa5bd02dfae0aba14fcea5
Mauricio Azevedo Franco Ladeira <mauricioafl001@gmail.com>
2022-12-13 15:38:43 -0300

    added yAxis number of commits

--- public/js/app.js
+++ public/js/app.js
@@ -21,7 +21,10 @@ btn.addEventListener("click", function (e) {
     // test.innerHTML = (get_commits_count_by_period())
 
     let xAxis = get_days_it_was_committed('Mauricio-AFLadeira', 'Tech-Challenge-Laravel-')
-    let yAxis = [1, 0, 4]
+    // let z = get_commits_per_day('Mauricio-AFLadeira', 'Tech-Challenge-Laravel-')
+    // console.log(z)
+    // debugger
+    let yAxis = get_commits_per_day('Mauricio-AFLadeira', 'Tech-Challenge-Laravel-')
 
     new Chart(ctx, {
         type: 'bar',
@@ -56,22 +59,40 @@ function httpGet(theUrl, return_headers) {
 }
 
 function get_commits_per_day(owner, repo) {
+    // let url = 'https://api.github.com/repos/Mauricio-AFLadeira/Tech-Challenge-Laravel-/commits?since=2022-12-08T00:00:00Z&until=2022-12-10T23:59:59Z'
     let url = base_url + '/repos/' + owner + '/' + repo + '/commits';
     let req = httpGet(url);
     let json = JSON.parse(req)
-    let days = get_days_it_was_committed(owner, repo)
+    let days = []
+    let daysNotUsed = []
+    let commitsPerDay = []
+    let count = 0
 
     for (let aux = 0; aux < json.length - 1; aux++) {
-
         let day = new Date(json[aux].commit.author.date).toISOString().replace('-', '/').split('T')[0].replace('-', '/')
-        if (day === days[aux]) {
-            oldDay = day
-            days.push(day)
-            // debugger
-        }
+        days.push(day)
+        // debugger
     }
 
-    return 1
+    for (let aux = 0; aux < days.length; aux++) {
+
+        // commitsPerDay = days.map(function () {
+        let day = new Date(json[aux].commit.author.date).toISOString().replace('-', '/').split('T')[0].replace('-', '/')
+        for (let index = 0; index < days.length; index++) {
+            if (day === days[index] && !daysNotUsed.includes(days[aux])) {
+                count++
+            }
+            // debugger
+        }
+
+        daysNotUsed.push(days[aux])
+        if (count != 0) {
+            commitsPerDay.push(count)
+        }
+        count = 0
+    }
+
+    return commitsPerDay.reverse()
 }
 
 function get_days_it_was_committed(owner, repo) {
