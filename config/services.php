995c229d90f370f34788df684a6330accae483cb
Mauricio Azevedo Franco Ladeira <mauricioafl001@gmail.com>
2022-12-14 23:18:09 -0300

    redirect .env route

--- config/services.php
+++ config/services.php
@@ -34,7 +34,7 @@ return [
     'github' => [
         'client_id' => env('CLIENT_ID'),
         'client_secret' => env('CLIENT_SECRET'),
-        'redirect' => 'http://127.0.0.1:8000/auth/github/callback',
+        'redirect' => env('GITHUB_REDIRECT'),
     ],
 
 ];
